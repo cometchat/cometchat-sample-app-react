4e8eaed0fa3426838a3bb4ced69cab3d57a14208
priyadarshini.cometchat <65010844+priyadarshininadar@users.noreply.github.com>
2021-10-04 21:15:35 +0530

    v3.0.1-2

--- postinstall.js
+++ postinstall.js
@@ -6,13 +6,13 @@ const rimraf = require("rimraf");
 const fileName = "cometchat-pro-react-ui-kit";
 const filePath = __dirname + "/src/" + fileName;
 
-const zipFileName = "cometchat-pro-react-ui-kit-3";
+const zipFileName = "cometchat-pro-react-ui-kit";
 
 const zipName = zipFileName + ".zip";
 const source = __dirname + "/" + zipFileName;
 const destination = filePath;
 
-const downloadUrl = "https://github.com/cometchat-pro/cometchat-pro-react-ui-kit/archive/v3.zip";
+const downloadUrl = "https://github.com/cometchat-pro/cometchat-pro-react-ui-kit/archive/master.zip";
 
 
 const download = (uri, filename, callback) => {
