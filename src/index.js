ece5d34bb89afab8fdaae9a6368826e2dfd5cc54
Mayur Bhandari <83179714+mayurbhandari10@users.noreply.github.com>
2022-07-07 15:49:13 +0530

    deploying

--- src/index.js
+++ src/index.js
@@ -71,4 +71,4 @@ CometChat.init(appID, appSetting).then(() => {
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-serviceWorker.register(`${process.env.PUBLIC_URL}/firebase-messaging-sw.js`);
+serviceWorker.register();
