77f7603d36cd7835eabec8f8ae3c1258829a6a4a
aaliya-shaikh <aaliya.shaikh@cometchat.com>
2022-12-13 19:22:02 +0530

    Updating emotion/core dependency

--- src/defaultPages/HomePage/index.js
+++ src/defaultPages/HomePage/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx } from "@emotion/react";
 
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
