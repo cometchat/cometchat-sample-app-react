8deb238b0403794d2586eb1fcce9a1383acfbb91
Juhi Saha <juhi.saha@inscripts.com>
2023-07-03 19:13:12 +0530

    fixes for calling

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./components/App";
 import { CometChatConstants } from "./constants"; 
-import { CometChat } from "@cometchat-pro/chat";
 import "@cometchat/uikit-elements";
-import { ChatConfigurator } from "@cometchat/chat-uikit-react";
+import { CometChatUIKit } from "@cometchat/chat-uikit-react";
+import { UIKitSettingsBuilder } from '@cometchat/uikit-shared';
 
 (async () => {
-  const appSettings = new CometChat.AppSettingsBuilder()
-                                   .subscribePresenceForAllUsers()
-                                   .setRegion(CometChatConstants.region)
-                                   .autoEstablishSocketConnection(true)
-                                   .build();
+  const uiKitSettings = new UIKitSettingsBuilder()
+  .setAppId(CometChatConstants.appId)
+  .setRegion(CometChatConstants.region)
+  .setAuthKey(CometChatConstants.authKey)
+  .subscribePresenceForFriends()
+  .build();
   try {
-    await CometChat.init(CometChatConstants.appId, appSettings);
+    await CometChatUIKit.init(uiKitSettings);
     console.log("Initialization completed successfully");
-    ChatConfigurator.init();
     const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
     root.render(<App />);
   }
