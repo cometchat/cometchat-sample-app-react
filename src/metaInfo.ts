f2526c625f76f89cffa3f7e0937257c156669c81
Nakul Jhunjhunwala <nakul.jhunjhunwala@cometchat.com>
2024-05-08 18:29:42 +0530

    Update Sample App for UiKit v4.3.7

--- src/metaInfo.ts
+++ src/metaInfo.ts
@@ -1,6 +1,6 @@
 export const metaInfo = {
-    name:"cometchat-chat-sample-app-react",
-    version:"4.3.4",
-    type:"sample",
-    platform:"React"
+    name: "cometchat-chat-sample-app-react",
+    version: "4.3.7",
+    type: "sample",
+    platform: "React"
 }
