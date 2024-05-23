122396f272c42602798c037b91b4312ba3831e4c
OmarAmeen01 <forcodingcoding@gmail.com>
2024-05-23 10:18:18 +0530

    changed its values so that the credentails remian private

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,7 +1,7 @@
 // Head over to the Credentials section in your app on CometChat Dashboard and where you can find your unique App ID, Region and Auth Key. 😊
 
 export const AppConstants = {
-  APP_ID: "APP_ID",
-  REGION: "REGION",
-  AUTH_KEY: "AUTH_KEY",
+  APP_ID: "Private",
+  REGION: "in",
+  AUTH_KEY: "Keeping it private as directed",
 };
