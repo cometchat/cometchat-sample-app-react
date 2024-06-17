b0966ce72d4bf1ea57b474ce6fa2131a27a9104e
Nakul Jhunjhunwala <nakul.jhunjhunwala@cometchat.com>
2024-06-17 17:34:26 +0530

    Update v3 Sample App

--- src/defaultPages/KitchenSinkApp/index.js
+++ src/defaultPages/KitchenSinkApp/index.js
@@ -1,116 +1,128 @@
+import * as actions from '../../store/action';
+
+import {
+  errorStyle,
+  inputWrapperStyle,
+  loginBtn,
+  subtitleStyle,
+  thumbnailWrapperStyle,
+  titleStyle,
+  uidWrapperStyle,
+  userContainerStyle,
+  userWrapperStyle,
+  wrapperStyle,
+} from "./style";
+
+import { COMETCHAT_CONSTANTS } from '../../consts';
+import { CometChatAvatar } from '../../cometchat-chat-uikit-react/CometChatWorkspace/src';
+import { Global } from "@emotion/react";
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
-import { Global } from "@emotion/react";
-
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-
-import { CometChatAvatar } from '../../cometchat-chat-uikit-react/CometChatWorkspace/src';
-import { COMETCHAT_CONSTANTS } from '../../consts';
-
-import {
-  wrapperStyle,
-  errorStyle,
-  titleStyle,
-  subtitleStyle,
-  userContainerStyle,
-  userWrapperStyle,
-  thumbnailWrapperStyle,
-  uidWrapperStyle,
-  inputWrapperStyle,
-  loginBtn,
-} from "./style";
-
 import { loaderStyle } from "./loader";
-
-import * as actions from '../../store/action';
+import {users} from "../../sampleApp/sampledata"
 
 class KitchenSinkApp extends React.PureComponent {
-
   constructor(props) {
     super(props);
 
     this.myRef = React.createRef();
+    this.state = {
+      userList: [],
+    };
+    this.fetchDefaultUsers.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDefaultUsers();
+  }
+
+  async fetchDefaultUsers() {
+    try {
+      const response = await fetch(
+        "https://assets.cometchat.io/sampleapp/sampledata.json"
+      );
+      const data = await response.json();
+      this.setState({
+        userList: data.users,
+      });
+    } catch (error) {
+      console.log("fetching default users failed, using fallback data", error);
+      this.setState({
+        userList: users.users,
+      });
+    }
   }
 
   login = (uid) => {
-
-    if(!uid) {
+    if (!uid) {
       uid = this.myRef.current.value;
     }
 
     this.uid = uid;
     this.props.onLogin(this.uid, COMETCHAT_CONSTANTS.AUTH_KEY);
-  }
+  };
 
   render() {
-
     let loader = null;
     if (this.props.loading) {
-      loader = (<div className="loading">Loading...</div>);
+      loader = <div className="loading">Loading...</div>;
     }
 
     let errorMessage = null;
     if (this.props.error) {
-      errorMessage = (<p css={errorStyle()}>{this.props.error.message}</p>);
+      errorMessage = <p css={errorStyle()}>{this.props.error.message}</p>;
     }
 
     let authRedirect = null;
     if (this.props.isLoggedIn) {
-      authRedirect = <Redirect to="/" />
+      authRedirect = <Redirect to="/" />;
     }
 
     return (
       <React.Fragment>
-      <Global styles={loaderStyle} />
-      <div css={wrapperStyle()}>
+        <Global styles={loaderStyle} />
+        <div css={wrapperStyle()}>
           {authRedirect}
           {loader}
           {errorMessage}
           <p css={titleStyle()}>Kitchen Sink App</p>
           <p css={subtitleStyle()}>Login with one of our sample users</p>
           <div css={userContainerStyle()}>
-            <div css={userWrapperStyle()} onClick={()=>this.login('superhero1')}>
-              <div css={thumbnailWrapperStyle()}>
-                <CometChatAvatar image='https://data-us.cometchat.io/assets/images/avatars/ironman.png' />
-              </div>
-              <p>superhero1</p>
-            </div>
-            <div css={userWrapperStyle()} onClick={()=>this.login('superhero2')}>
-              <div css={thumbnailWrapperStyle()}>
-                <CometChatAvatar image='https://data-us.cometchat.io/assets/images/avatars/captainamerica.png' />
-              </div>
-              <p>superhero2</p>
-            </div>
-            <div css={userWrapperStyle()} onClick={()=>this.login('superhero3')}>
-              <div css={thumbnailWrapperStyle()}>
-                <CometChatAvatar image='https://data-us.cometchat.io/assets/images/avatars/spiderman.png' />
-              </div>
-              <p>superhero3</p>
-            </div>
-            <div css={userWrapperStyle()} onClick={()=>this.login('superhero4')}>
-              <div css={thumbnailWrapperStyle()}>
-                <CometChatAvatar image='https://data-us.cometchat.io/assets/images/avatars/wolverine.png' />
-              </div>
-              <p>superhero4</p>
-            </div>
-            <div css={userWrapperStyle()} onClick={()=>this.login('superhero5')}>
-              <div css={thumbnailWrapperStyle()}>
-                <CometChatAvatar image='https://data-us.cometchat.io/assets/images/avatars/cyclops.png' />
-              </div>
-              <p>superhero5</p>
-            </div>
-          </div><br/>
+            {this.state?.userList.map((user, key) => {
+              return (
+                <div
+                  css={userWrapperStyle()}
+                  onClick={() => this.login(user.uid)}
+                >
+                  <div css={thumbnailWrapperStyle()}>
+                    <CometChatAvatar image={user.avatar} />
+                  </div>
+                  <p>{user.name}</p>
+                </div>
+              );
+            })}
+          </div>
+          <br />
           <div css={uidWrapperStyle()}>
             <div>
               <p css={subtitleStyle()}>Login with UID</p>
             </div>
             <div css={inputWrapperStyle()}>
-              <input ref={this.myRef} type="text" placeholder="Enter your UID here" />
+              <input
+                ref={this.myRef}
+                type="text"
+                placeholder="Enter your UID here"
+              />
             </div>
-            <div css={loginBtn()}><button type="button" onClick={() => this.login()}>Login</button></div>
+            <div css={loginBtn()}>
+              <button type="button" onClick={() => this.login()}>
+                Login
+              </button>
+            </div>
           </div>
         </div>
       </React.Fragment>
---
+++ src/sampleApp/sampledata.js
@@ -0,0 +1,35 @@
+import CaptainAmerica from "./assets/captainamerica_avatar.png";
+import Cyclops from "./assets/cyclops_avatar.png";
+import IronMan from "./assets/ironman_avatar.png";
+import Spiderman from "./assets/spiderman_avatar.png";
+import Wolverine from "./assets/wolverine_avatar.png";
+
+export const users = {
+  users: [
+    {
+      uid: "superhero1",
+      name: "Iron Man",
+      avatar: IronMan,
+    },
+    {
+      uid: "superhero2",
+      name: "Captain America",
+      avatar: CaptainAmerica,
+    },
+    {
+      uid: "superhero3",
+      name: "Spiderman",
+      avatar: Spiderman,
+    },
+    {
+      uid: "superhero4",
+      name: "Wolverine",
+      avatar: Wolverine,
+    },
+    {
+      uid: "superhero5",
+      name: "Cyclops",
+      avatar: Cyclops,
+    },
+  ],
+};
